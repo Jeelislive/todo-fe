f17da5bb8e94109267a12a5ec9ff9c052350f65e
JEEL RUPARELIYA <108856045+Jeelislive@users.noreply.github.com>
2024-06-06 13:33:05 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
         body: JSON.stringify({ email, password }),
        
       }
-      const response = await fetch('https://todo-be-peach.vercel.app/login', option);
+      const response = await fetch('https://todo-fe-mu.vercel.app/login', option);
       if (response.ok) {
         navigate('/dashboard');
       } else {
