91b5b792ce3fbc872232f53669df9e7ee4a97829
JEEL RUPARELIYA <108856045+Jeelislive@users.noreply.github.com>
2024-06-13 11:09:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-// import Dashboard from './pages/Dashboard';
+ import Dashboard from './pages/Dashboard';
 import Logout from './pages/Logout';
 
 
@@ -17,7 +17,7 @@ function App() {
         <Route path='/' element={ <Home /> } />
         <Route path='/login' element={ <Login /> } />
         <Route path='/register' element={ <Register /> } />
-{/*         <Route path='/dashboard' element={ <Dashboard /> } /> */}
+        <Route path='/dashboard' element={ <Dashboard /> } />
       </Routes>
     </>
   )
