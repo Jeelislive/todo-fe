602dcb71c0b1e707741a7a5956f9b5172fc26663
JEEL RUPARELIYA <108856045+Jeelislive@users.noreply.github.com>
2024-06-18 13:18:19 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from "react-router-dom";
-import '../Login.css'; // Make sure you import the CSS file
+
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ errorMessage, setErrorMessage ] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -19,7 +19,8 @@ const Login = () => {
         body: JSON.stringify({ email, password }),
       }
       const baseUrl = process.env.NODE_ENV === 'production' ? 'https://todo-be-peach.vercel.app' : '';
-      const response = await fetch(`${baseUrl}/api/login`, option);
+
+    const response = await fetch(`${baseUrl}/api/login`, option);
       if (response.ok) {
         navigate('/dashboard');
       } else {
@@ -33,39 +34,73 @@ const Login = () => {
   };
 
   return (
-    <div className="main">
-      <div className="login">
-        <form className="form" onSubmit={handleSubmit}>
-          <label htmlFor="chk" aria-hidden="true">Log in</label>
-          <input
-            className="input"
-            type="email"
-            name="email"
-            placeholder="Email"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            className="input"
-            type="password"
-            name="pswd"
-            placeholder="Password"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {errorMessage && (
-            <div className="error-message" role="alert">
-              <strong>Error:</strong>
-              <span>{errorMessage}</span>
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+        <form onSubmit={ handleSubmit } className="mt-8 space-y-6" action="#" method="POST">
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                value={ email }
+                onChange={ (e) => setEmail(e.target.value) }
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+              />
+
             </div>
-          )}
-          <button>Log in</button>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="password"
+                required
+                value={ password }
+                onChange={ (e) => setPassword(e.target.value) }
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+              />
+
+            </div>
+          </div>
+
+          { errorMessage && (
+            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+              <strong className="font-bold">Error:</strong>
+              <span className="block sm:inline">{ errorMessage }</span>
+              <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+              </span>
+            </div>
+          ) }
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Login
+            </button>
+          </div>
         </form>
         <div className="text-center">
           <p className="mt-2 text-center text-sm text-gray-600">
-            Don't have an account?{' '}
+            Don't have an account?{ ' ' }
             <Link to="/register" className="font-medium text-indigo-600 hover:text-indigo-500">
               Sign up
             </Link>
